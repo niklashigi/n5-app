584e8cb74893c6917b908c09787ced9f7eaad296
Niklas Higi <niklas@kombo.dev>
2023-11-14 21:26:17 +0100

    Set up `unplugin-icons` and clean up UI

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -24,10 +24,10 @@ function App() {
     <trpc.Provider queryClient={queryClient} client={trpcClient}>
       <QueryClientProvider client={queryClient}>
         <div className="max-w-xl mx-auto">
-          <div className="text-center text-3xl font-bold text-gray-700">
+          <div className="py-8 text-center text-3xl font-bold text-gray-700">
             <h1>N5 App</h1>
           </div>
-          <div className="max-w-md mx-auto flex flex-col gap-y-4 mt-8">
+          <div className="max-w-md mx-auto flex flex-col gap-y-4">
             <ListTodos />
             <AddTodo />
           </div>
--- packages/client/src/components/ListTodos.tsx
+++ packages/client/src/components/ListTodos.tsx
@@ -1,4 +1,9 @@
 import { trpc } from "../lib/trpc";
+import clsx from "clsx";
+
+import IconX from "~icons/tabler/x";
+import IconSquare from "~icons/tabler/square";
+import IconSquareCheckFilled from "~icons/tabler/square-check-filled";
 
 export default function ListTodos() {
   function handleDelete() {}
@@ -18,17 +23,15 @@ export default function ListTodos() {
   }
 
   return (
-    <ul className="">
+    <ul className="flex flex-col gap-3">
       {response.data.map((todo) => {
         return (
           <li
-            className="flex justify-between items-center bg-gray-100 py-2 px-3 rounded-md space-x-2 my-1"
+            className="flex justify-between items-center bg-gray-100 py-2 px-3 rounded-md space-x-2"
             key={todo.id}
           >
-            <p className="flex-grow">{todo.title}</p>
-
             <button
-              className="text-white bg-green-600 px-2 py-1 rounded text-sm hover:line-through cursor-pointer hover:text-black"
+              className="rounded text-sm hover:line-through cursor-pointer hover:text-black"
               onClick={() =>
                 updateMutation.mutate(
                   { id: todo.id, isCompleted: !todo.isCompleted },
@@ -36,8 +39,23 @@ export default function ListTodos() {
                 )
               }
             >
-              {todo.isCompleted ? "Complete" : "Incomplete"}
+              {todo.isCompleted ? (
+                <IconSquareCheckFilled className="w-6 h-6 text-gray-400" />
+              ) : (
+                <IconSquare className="w-6 h-6 text-gray-500" />
+              )}
             </button>
+
+            <p
+              className={clsx(
+                "flex-grow",
+                todo.isCompleted
+                  ? "line-through text-gray-500"
+                  : "text-gray-900",
+              )}
+            >
+              {todo.title}
+            </p>
 
             <button
               onClick={() =>
@@ -50,22 +68,9 @@ export default function ListTodos() {
                   },
                 )
               }
-              className="text-red-500 hover:text-white hover:bg-red-500 p-1 rounded"
+              className="text-gray-300 hover:text-white hover:bg-red-500 p-1 rounded"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 "
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
+              <IconX />
             </button>
           </li>
         );
