ed887b1f3b0608688f27389297d8ea04f83585fc
Niklas Higi <niklas@kombo.dev>
2023-11-14 21:04:40 +0100

    Remove cruft

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,35 +1,33 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
-import { useState } from "react";
-import "./App.css";
-import expressLogo from "./assets/express.png";
-import reactLogo from "./assets/react.svg";
-import tRpcLogo from "./assets/trpc.svg";
+import { useMemo } from "react";
+
 import AddTodo from "./components/AddTodo";
 import ListTodos from "./components/ListTodos";
 import { trpc } from "./lib/trpc";
 
 function App() {
-  const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() => {
-    return trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: "http://localhost:3000/trpc",
-        }),
-      ],
-    });
-  });
+  const queryClient = useMemo(() => new QueryClient(), []);
+  const trpcClient = useMemo(
+    () =>
+      trpc.createClient({
+        links: [
+          httpBatchLink({
+            url: "http://localhost:3000/trpc",
+          }),
+        ],
+      }),
+    [],
+  );
 
   return (
     <trpc.Provider queryClient={queryClient} client={trpcClient}>
       <QueryClientProvider client={queryClient}>
         <div className="max-w-xl mx-auto">
           <div className="text-center text-3xl font-bold text-gray-700">
-            <h1>Vite + React | Express | tRPC</h1>
-            <h3>npm workspaces</h3>
+            <h1>N5 App</h1>
           </div>
-          <div className="max-w-md mx-auto grid gap-y-4 mt-8">
+          <div className="max-w-md mx-auto flex flex-col gap-y-4 mt-8">
             <ListTodos />
             <AddTodo />
           </div>
