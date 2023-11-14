4fcb8c45234c5f62872d7c28204080771ae2860a
Niklas Higi <niklas@kombo.dev>
2023-11-14 20:55:45 +0100

    Improve todos UX

--- packages/client/src/components/AddTodo.tsx
+++ packages/client/src/components/AddTodo.tsx
@@ -7,28 +7,33 @@ export default function AddTodo() {
   const trpcContext = trpc.useContext();
 
   return (
-    <form className="flex justify-between space-x-4">
+    <form
+      className="flex justify-between space-x-4"
+      onSubmit={(event) => {
+        event.preventDefault();
+        addTodoMutation.mutate(
+          { title: title },
+          {
+            onSuccess: () => {
+              console.log("created a todo");
+              trpcContext.todo.list.invalidate();
+              setTitle("");
+            },
+          },
+        );
+      }}
+    >
       <input
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         type="text"
         placeholder="Get milk..."
         className="flex-grow rounded-md"
+        required
       />
       <button
         type="submit"
         className="bg-blue-500 hover:bg-blue-600 active:bg-blue-500 text-white py-1 px-3 rounded-md"
-        onClick={() => {
-          addTodoMutation.mutate(
-            { title: title },
-            {
-              onSuccess: () => {
-                console.log("created a todo");
-                trpcContext.todo.list.invalidate();
-              },
-            },
-          );
-        }}
       >
         Add todo
       </button>
